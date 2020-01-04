567fd55ad8159a26e5a24f0014863ce373611b25
Noam Ross (the Robot Version) <ross@ecohealthalliance.org>
2020-01-04 16:07:42 -0500

    Install GPG prior to R so we can check the CRAN repo

--- scripts/install_R.sh
+++ scripts/install_R.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 set -e
 
-echo "deb http://cloud.r-project.org/bin/linux/ubuntu ${UBUNTU_VERSION}-cran35/" >> /etc/apt/sources.list
-gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | apt-key add - 
 apt-get update
 apt-get -y install --no-install-recommends \
       ca-certificates \
       less \
-      littler \
       libopenblas-base \
       locales \
-      r-base-dev \
       vim-tiny \
-      wget
+      wget \
+      dirmngr \
+      gpg \
+      gpg-agent
+echo "deb http://cloud.r-project.org/bin/linux/ubuntu ${UBUNTU_VERSION}-cran35/" >> /etc/apt/sources.list
+gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | apt-key add -
+apt-get update
+apt-get -y install --no-install-recommends \
+      r-base-dev \
+      littler 
 rm -rf /var/lib/apt/lists/*
 
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
