9d10f9a1331959cd0eebe772fd71d070d43a65c4
Noam Ross (the Robot Version) <ross@ecohealthalliance.org>
2020-01-26 13:49:42 -0500

    Automatically determine image order in build-images

--- build-images.R
+++ build-images.R
@@ -1,10 +1,32 @@
 #!/usr/bin/env Rscript
 
 library(stevedore)
-dc <- docker_client()
+library(igraph)
 
-dockerfiles <- list.files("dockerfiles", full.names = TRUE)
-tags <- gsub("(dockerfiles/Dockerfile_)(.*)_(.*)$", "rocker/\\2:\\3", dockerfiles, perl = TRUE)
+dc <- docker_client()
+images_list <- jsonlite::read_json("versions.json")
+tags <- 
+  vapply(images_list, 
+         function(.) paste0("rocker/", .$ROCKER_IMAGE, ":", .$ROCKER_TAG),
+         character(1))
+names(images_list) <- tags
+names(tags) <- tags
+
+from <- vapply(images_list, function(.) .$FROM_IMAGE, character(1))
+image_graph <- igraph::graph_from_data_frame(cbind(from, tags))
+tag_order <- grep("^rocker", names(igraph::topo_sort(image_graph)), value = TRUE)
+
+
+images_list <- images_list[tag_order]
+tags <- tags[tag_order]
+
+dockerfiles <- 
+  file.path(
+    "dockerfiles",
+    vapply(images_list,
+           function(.) paste0("Dockerfile_", .$ROCKER_IMAGE, "_", .$ROCKER_TAG),
+           character(1))
+  )
 
 build_fn <- function(dockerfile, tag) {
   dc$image$build(dockerfile = dockerfile, tag = tag, context = ".")
