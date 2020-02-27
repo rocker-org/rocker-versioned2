06b3ba68c43b73fc8e2463c2a9c880f088c18dc6
Carl Boettiger <cboettig@gmail.com>
2020-02-27 20:26:13 +0000

    tags for gh

--- create-compose.R
+++ create-compose.R
@@ -2,34 +2,48 @@ library(yaml)
 library(jsonlite)
 library(purrr)
 
-json <- read_json("versions-cuda.json")
 
-prefix <- "dockerfiles/Dockerfile_"
-name <- map_chr(json, "ROCKER_IMAGE")
-tag <-  map_chr(json, "ROCKER_TAG")
-dockerfiles <- paste0(prefix, name, "_", tag)
-names(dockerfiles) <- name
+write_compose <- 
+  function(json_file, out = "docker-compose.yml", org = "rocker"){
+  
+  json <- jsonlite::read_json(json_file)
+  prefix <- "dockerfiles/Dockerfile_"
+  name <- purrr::map_chr(json, "ROCKER_IMAGE")
+  tag <-  purrr::map_chr(json, "ROCKER_TAG")
+  dockerfiles <- paste0(prefix, name, "_", tag)
+  names(dockerfiles) <- name
+  
+  image_name <- function(d, org){
+    x <- gsub(prefix, "", d)
+    x <- gsub("_", ":", x)
+    paste(org, x, sep="/")
+  }
+  
+  
+  services <- lapply(dockerfiles, function(d) 
+    list(image = image_name(d, org),
+         build = list(
+           context = ".",
+           dockerfile = d
+           )
+         )
+  )
+  compose <- list(
+    version = "3",
+    services =  services
+  )
+  yaml::write_yaml(compose, out)
 
-image_name <- function(d){
-  x <- gsub(prefix, "", d)
-  x <- gsub("_", ":", x)
-  paste0("rocker/", x)
 }
 
-services <- lapply(dockerfiles, function(d) 
-  list(image = image_name(d),
-       build = list(
-         context = ".",
-         dockerfile = d
-         )
-       )
-)
 
-compose <- list(
-  version = "3",
-  services =  services
-)
 
-write_yaml(compose, "docker-compose.yml")
+write_compose("versions-cuda.json", "docker-compose.yml", org = "rocker")
+write_compose("versions-cuda.json", "docker-compose-gh.yml", 
+              org = "docker.pkg.github.com/noamross/rocker-versioned2")
+
+
+
+
 
 
