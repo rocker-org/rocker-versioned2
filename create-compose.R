e50fc8a9cd49e074247fc7fc70f7580f49ade1e4
Carl Boettiger <cboettig@gmail.com>
2020-02-27 19:10:36 +0000

    proof-of-concept

---
+++ create-compose.R
@@ -0,0 +1,20 @@
+library(yaml)
+library(jsonlite)
+library(purrr)
+
+json <- read_json("versions-cuda.json")
+
+name <- map_chr(json, "ROCKER_IMAGE")
+tag <-  map_chr(json, "ROCKER_TAG")
+dockerfiles <- paste0("dockerfiles/Dockerfile_", name, "_", tag)
+names(dockerfiles) <- name
+services <- lapply(dockerfiles, function(d) list(dockerfile = d))
+
+compose <- list(
+  version = "3",
+  services =  services
+)
+
+write_yaml(compose, "compose.yml")
+
+
