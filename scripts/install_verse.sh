d49ad6139718129a9be8f676d38e42a6e00ef4f1
Carl Boettiger <cboettig@gmail.com>
2020-02-28 08:45:43 -0800

    fix reversion

--- scripts/install_verse.sh
+++ scripts/install_verse.sh
@@ -33,22 +33,10 @@ wget "https://travis-bin.yihui.name/texlive-local.deb" \
 
 install2.r --error -r $CRAN --skipinstalled tinytex
 
-## Admin-based install of TinyTeX:
-wget -qO- \
-    "https://github.com/yihui/tinytex/raw/master/tools/install-unx.sh" | \
-    sh -s - --admin --no-path \
-  && mv ~/.TinyTeX /opt/TinyTeX \
-  && /opt/TinyTeX/bin/*/tlmgr path add \
-  && tlmgr install metafont mfware inconsolata tex ae parskip listings \
-  && tlmgr path add \
-  && Rscript -e "tinytex::r_texmf()" \
-  && chown -R root:staff /opt/TinyTeX \
-  && chown -R root:staff ${R_HOME}/site-library \
-  && chmod -R g+w /opt/TinyTeX \
-  && chmod -R g+wx /opt/TinyTeX/bin \
-  && echo "PATH=${PATH}" >> ${R_HOME}/etc/Renviron
- 
+/rocker_scripts/install_texlive.sh
 
+echo "PATH=${PATH}" >> ${R_HOME}/etc/Renviron
+ 
 install2.r --error --deps TRUE -r $CRAN --skipinstalled \
     blogdown bookdown rticles rmdshower rJava
 
