d21c6c0d3e02a8b45f355f0d16f459f50573b32e
Carl Boettiger <cboettig@gmail.com>
2020-03-11 19:18:16 +0000

    enforce build order via depends_on
    p.s. maybe the whole compaction logic for the depends on case could be re-worked to be much mor elegant!

--- write_compose.R
+++ write_compose.R
@@ -5,46 +5,55 @@ library(yaml)
 
 write_compose <- 
   function(json_file, out = "docker-compose.yml", org = "rocker"){
-  
-  json <- yaml::read_yaml(json_file) #jsonlite::read_json(json_file)
-  prefix <- "dockerfiles/Dockerfile_"
-  
-  map_chr <- function(x, name) vapply(x, `[[`, character(1L), name)
-
-  name <- map_chr(json, "ROCKER_IMAGE")
-  tag <-  map_chr(json, "ROCKER_TAG")
-  
-  
-  dockerfiles <- paste0(prefix, name, "_", tag)
-  names(dockerfiles) <- name
-  
-  image_name <- function(d, org){
-    x <- gsub(prefix, "", d)
-    x <- gsub("_", ":", x)
-    paste(org, x, sep="/")
+    
+    json <- yaml::read_yaml(json_file) #jsonlite::read_json(json_file)
+    prefix <- "dockerfiles/Dockerfile_"
+    map_chr <- function(x, name) vapply(x, `[[`, character(1L), name)
+    
+    name <- map_chr(json, "ROCKER_IMAGE")
+    tag <-  map_chr(json, "ROCKER_TAG")
+    
+    dockerfiles <- paste0(prefix, name, "_", tag)
+    names(dockerfiles) <- name
+    
+    image_name <- function(d, org){
+      x <- gsub(prefix, "", d)
+      x <- gsub("_", ":", x)
+      paste(org, x, sep="/")
+    }
+    
+    depends_on <- c("", name[1:(length(name)-1)])
+    not_blank <- function(x) if(x == "") return(NULL) else x
+    is_empty <- function(x) length(x) == 0 || is.null(x)
+    compact <- function (l) Filter(Negate(is_empty), l)
+    
+    services <- vector("list", length(dockerfiles))
+    names(services) <- name
+    for(i in seq_along(dockerfiles)){
+      d <- dockerfiles[[i]] 
+      services[[i]] <- compact(list(
+        image = image_name(d, org),
+        depends_on = compact(list(not_blank(depends_on[i]))),
+        build = list(
+          context = ".",
+          dockerfile = d
+        )
+      ))
+    }
+    
+    
+    compose <- list(
+      version = "3",
+      services =  services
+    )
+    yaml::write_yaml(compose, out)
+    
   }
-  
-  
-  services <- lapply(dockerfiles, function(d) 
-    list(image = image_name(d, org),
-         build = list(
-           context = ".",
-           dockerfile = d
-           )
-         )
-  )
-  compose <- list(
-    version = "3",
-    services =  services
-  )
-  yaml::write_yaml(compose, out)
-
-}
 
 
 
-write_compose("versions-test.json", "docker-compose.yml", org = "rocker")
-write_compose("versions-test.json", "docker-compose-gh-registry.yml", 
+write_compose("versions-cuda.json", "docker-compose.yml", org = "rocker")
+write_compose("versions-cuda.json", "docker-compose-gh.yml", 
               org = "docker.pkg.github.com/noamross/rocker-versioned2")
 
 
