753bdc7d0f70fb9f7bc55d870ce35e060c44e421
Carl Boettiger <cboettig@gmail.com>
2020-03-25 17:13:10 -0700

    fix context

--- write_compose.R
+++ write_compose.R
@@ -38,7 +38,7 @@ write_compose <-
         image = image_name(d, org),
         depends_on = compact(list(not_blank(depends_on[i]))),
         build = list(
-          context = ".",
+          context = "..",
           dockerfile = d
         )
       ))
@@ -63,11 +63,10 @@ write_compose("stacks/extensions-3.6.3.json", "compose/extensions-3.6.3.yml", or
 
 
 ## rockerdev org is just for testing!
-write_compose("stacks/core-3.6.3-gpu.json", "compose/rockerdev/core-3.6.3-gpu.yml",   org = "rockerdev")
-write_compose("stacks/extensions-3.6.3-gpu.json", "compose/rockerdev/extensions-3.6.3-gpu.yml", org = "rockerdev")
-
-write_compose("stacks/core-3.6.3.json", "compose/rockerdev/core-3.6.3.yml",   org = "rockerdev")
-write_compose("stacks/extensions-3.6.3.json", "compose/rockerdev/extensions-3.6.3.yml", org = "rockerdev")
+write_compose("stacks/core-3.6.3-gpu.json", "compose-rockerdev/core-3.6.3-gpu.yml",   org = "rockerdev")
+write_compose("stacks/extensions-3.6.3-gpu.json", "compose-rockerdev/extensions-3.6.3-gpu.yml", org = "rockerdev")
+write_compose("stacks/core-3.6.3.json", "compose-rockerdev/core-3.6.3.yml",   org = "rockerdev")
+write_compose("stacks/extensions-3.6.3.json", "compose-rockerdev/extensions-3.6.3.yml", org = "rockerdev")
 
 
 
