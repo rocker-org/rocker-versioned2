70aa32eb08bedd14e10a8e3a40c174b46d401790
Michael Anselmi <ma.anselmi@gmail.com>
2020-05-13 11:47:03 -0400

    Fixup install_shiny_server script and dependency scripts
    Past versions of the `rocker/rstudio` image had a `/etc/cont-init.d/add`
    script that made it easy to automatically install and configure Shiny
    Server. I recently noticed that this script has been replaced by
    `/rocker_scripts/install_shiny_server.sh`, so I tried running it.
    
    However, the dependency script `install_pandoc.sh` failed due to
    assumptions that the script would only be one run once. I've made
    minimal changes to allow the script to be re-run and trimmed whitespace.
    
    Also, I noticed a bug in `install_s6init.sh` where the string literal
    `S6_VERSION` was being written to `/rocker_scripts/.s6_version` instead
    of the value of the `S6_VERSION` variable. I've fixed that as well, and
    also trimmed whitespace.
    
    I also addressed several issues with `install_shiny_server.sh`,
    including fixing `shiny.shiny` → `shiny:shiny` in a `chown` command,
    replacing a multi-line `echo` command with some unexpected substitutions
    with `cat`-ing a heredoc, and removing a bare stdout redirection
    operator that had no destination specified. Also, I trimmed whitespace.
    :)
    
    After making these changes, I was able to get Shiny Server working
    within `rocker/rstudio`.

--- scripts/install_s6init.sh
+++ scripts/install_s6init.sh
@@ -13,11 +13,10 @@ if [ -f "/rocker_scripts/.s6_version" ] && [ "$S6_VERSION" = "$(cat /rocker_scri
   echo "S6 already installed"
 else
   wget -P /tmp/ https://github.com/just-containers/s6-overlay/releases/download/${S6_VERSION}/s6-overlay-amd64.tar.gz
-  
-  ## need the modified double tar now, see https://github.com/just-containers/s6-overlay/issues/288 
+
+  ## need the modified double tar now, see https://github.com/just-containers/s6-overlay/issues/288
   tar hzxf /tmp/s6-overlay-amd64.tar.gz -C / --exclude=usr/bin/execlineb
   tar hzxf /tmp/s6-overlay-amd64.tar.gz -C /usr ./bin/execlineb && $_clean
 
-  echo "S6_VERSION" > /rocker_scripts/.s6_version
+  echo "$S6_VERSION" > /rocker_scripts/.s6_version
 fi
-
