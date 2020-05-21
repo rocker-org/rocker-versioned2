e727b20c05b81c0f6362a95c0047a6ff896756a4
Carl Boettiger <cboettig@gmail.com>
2020-05-20 17:27:49 -0700

    don't include callr overhead in timing

--- tests/ml/nvblas.R
+++ tests/ml/nvblas.R
@@ -1,10 +1,12 @@
 install.packages("callr")
 
-system.time(
+
 callr::r(function(){
+  system.time({
   N <- 2^14
   M <- matrix(rnorm(N*N), nrow=N, ncol=N)
   M %*% M
+  })  
   }, env = c(LD_PRELOAD="libnvblas.so"))
 )
 
