93ded8614fc6edba8e646116bffabe9c33607ec2
Carl Boettiger <cboettig@gmail.com>
2020-07-07 17:14:04 -0700

    Update rsession.sh

--- scripts/rsession.sh
+++ scripts/rsession.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-R_DOC_DIR=/usr/share/R/doc
-R_INCLUDE_DIR=/usr/share/R/include
-R_SHARE_DIR=/usr/share/R/share
+R_DOC_DIR=$R_HOME/doc
+R_INCLUDE_DIR=$R_HOME/include
+R_SHARE_DIR=$R_HOME/share
 RSTUDIO_DEFAULT_R_VERSION_HOME=$R_HOME
-RSTUDIO_DEFAULT_R_VERSION=3.6.3
+RSTUDIO_DEFAULT_R_VERSION=$R_VERSION
 PATH=$PATH:/usr/lib/rstudio-server/bin
 rsession --standalone=1 \
          --program-mode=server \
