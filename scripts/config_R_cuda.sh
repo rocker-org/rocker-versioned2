8776d9a3c8322051910ac470a4fbfb5cb0e9f660
Rob Young <young@ecohealthalliance.org>
2021-09-08 16:12:05 -0400

    bugfix and added proposed tests to config

--- scripts/config_R_cuda.sh
+++ scripts/config_R_cuda.sh
@@ -26,10 +26,42 @@ chmod a+rw /var/log/nvblas.log
 ## Configure R & RStudio to use drop-in CUDA blas
 ## Allow R to use CUDA for BLAS, with fallback on openblas
 echo 'NVBLAS_LOGFILE /var/log/nvblas.log
-NVBLAS_CPU_BLAS_LIB /usr/lib/x86_64-linux-gnu/openblas/libblas.so.3
+NVBLAS_CPU_BLAS_LIB /usr/lib/x86_64-linux-gnu/openblas-pthread/libblas.so.3
 NVBLAS_GPU_LIST ALL' > /etc/nvblas.conf
 
 echo "NVBLAS_CONFIG_FILE=$NVBLAS_CONFIG_FILE" >> ${R_HOME}/etc/Renviron.site
+
+## Test GPU fuctionality
+TEST_FAIL=false
+# driver
+PROC_DRIVER_FILE=/proc/driver/nvidia/version
+if [ ! -f "$PROC_DRIVER_FILE" ]
+then
+  echo "$PROC_DRIVER_FILE doesn't exist" | tee -a /var/log/nvblas.log
+  echo "WARNING: CUDA driver may not be correctly installed." | tee -a /var/log/nvblas.log
+  TEST_FAIL=true
+fi
+# toolkit
+TOOLKIT_CHECK_OUTPUT=$(nvcc -V 2>&1)
+if [ $? -ne 0 ]
+then
+  echo "Failed to run 'nvcc -V' with error message: $TOOLKIT_CHECK_OUTPUT" | tee -a /var/log/nvblas.log
+  echo "WARNING: CUDA toolkit may not be correctly installed." | tee -a /var/log/nvblas.log
+  TEST_FAIL=true
+fi
+# tensorflow and nvblas
+NVBLAS_OUTPUT=$(Rscript ../tests/gpu/misc/nvblas.R 2>&1)
+if [ $? -ne 0 ]
+then
+  echo "Failed nvBLAS test with error $NVBLAS_OUTPUT" | tee -a /var/log/nvblas.log
+  TEST_FAIL=true
+fi
+if [ "$TEST_FAIL" = true ]
+then
+  echo "WARNING: at least one of the GPU functionality tests has failed." | tee -a /var/log/nvblas.log
+  echo "Please run rocker-versioned2/tests/gpu/test-gpu.sh script for more detailed information." | tee -a /var/log/nvblas.log
+fi
+## end GPU tests
 
 ## We don't want to set LD_PRELOAD globally
 ##ENV LD_PRELOAD=/usr/local/cuda/lib64/libnvblas.so
--- tests/gpu/misc/cuda_ver.awk
+++ tests/gpu/misc/cuda_ver.awk
@@ -3,12 +3,12 @@
   if (container_name == "rocker_cuda11"){
     cmd = sprintf("docker exec %s /test-gpu.sh 11 /root/test-gpu.log", $1)
     system(cmd)
-    cmd = sprintf("docker cp %s:/root/test-gpu.log /", $1)
+    cmd = sprintf("docker cp %s:/root/test-gpu.log /test-gpu-11.log", $1)
     system(cmd)
   }else if (container_name == "rocker_cuda10"){
     cmd = sprintf("docker exec %s /test-gpu.sh 10 /root/test-gpu.log", $1)
     system(cmd)
-    cmd = sprintf("docker cp %s:/root/test-gpu.log /", $1)
+    cmd = sprintf("docker cp %s:/root/test-gpu.log /test-gpu-10.log", $1)
     system(cmd)
   }
 }
