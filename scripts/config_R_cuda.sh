883358950f09d1125ab7ad44d766abc63c669263
Rob Young <young@ecohealthalliance.org>
2021-09-21 13:54:29 -0400

    removed tests from config

--- scripts/config_R_cuda.sh
+++ scripts/config_R_cuda.sh
@@ -31,59 +31,6 @@ NVBLAS_GPU_LIST ALL' > /etc/nvblas.conf
 
 echo "NVBLAS_CONFIG_FILE=$NVBLAS_CONFIG_FILE" >> ${R_HOME}/etc/Renviron.site
 
-## Test GPU fuctionality
-TEST_FAIL=false
-# driver
-PROC_DRIVER_FILE=/proc/driver/nvidia/version
-if [ ! -f "$PROC_DRIVER_FILE" ]
-then
-  echo "$PROC_DRIVER_FILE doesn't exist" | tee -a /var/log/nvblas.log
-  echo "WARNING: CUDA driver may not be correctly installed." | tee -a /var/log/nvblas.log
-  TEST_FAIL=true
-fi
-# toolkit
-TOOLKIT_CHECK_OUTPUT=$(nvcc -V 2>&1)
-if [ $? -ne 0 ]
-then
-  echo "Failed to run 'nvcc -V' with error message: $TOOLKIT_CHECK_OUTPUT" | tee -a /var/log/nvblas.log
-  echo "WARNING: CUDA toolkit may not be correctly installed." | tee -a /var/log/nvblas.log
-  TEST_FAIL=true
-fi
-# nvblas
-NVBLAS_OUTPUT=$(Rscript ../tests/gpu/misc/nvblas.R 2>&1)
-if [ $? -ne 0 ]
-then
-  echo "Failed nvBLAS test with error $NVBLAS_OUTPUT" | tee -a /var/log/nvblas.log
-  TEST_FAIL=true
-fi
-# tensorflow
-TF_OUTPUT=$(Rscript ../tests/gpu/misc/examples_tf.R 2>&1)
-if [ $? -ne 0 ]
-then
-  echo "Failed tensorflow test with error $TF_OUTPUT" | tee -a /var/log/nvblas.log
-else
-  GPU_STR="Created device /job:localhost/replica:0/task:0/device:GPU:0"
-  if [[ "$TF_OUTPUT" == *"$GPU_STR"* ]]
-  then
-    echo "tensorflow GPU test succeeded" | tee -a /var/log/nvblas.log
-    echo "output in the log" | tee -a /var/log/nvblas.log
-    echo $TF_OUTPUT >> /var/log/nvblas.log
-  else
-    echo "CPU tensorflow test succeeded" | tee -a /var/log/nvblas.log
-    echo "Failed GPU tensorflow test. See log for details." | tee -a /var/log/nvblas.log
-    echo $TF_OUTPUT >> /var/log/nvblas.log
-  fi
-fi
-
-
-
-if [ "$TEST_FAIL" = true ]
-then
-  echo "WARNING: at least one of the GPU functionality tests has failed." | tee -a /var/log/nvblas.log
-  echo "Please run rocker-versioned2/tests/gpu/test-gpu.sh script for more detailed information." | tee -a /var/log/nvblas.log
-fi
-## end GPU tests
-
 ## We don't want to set LD_PRELOAD globally
 ##ENV LD_PRELOAD=/usr/local/cuda/lib64/libnvblas.so
 #
