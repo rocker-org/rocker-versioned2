054f1d57ad36c7da2021fd6ca03c577d272f1cc1
eitsupi <ts1s1andn@gmail.com>
2021-10-20 19:55:10 +0900

    Complete rewrite to match other rocker scripts.
    - Multi-architecture support
    - Automatically get the latest version of Julia
    - Create a symbolic link instead of changing the PATH environment variable
    - Omit installation of Julia packages

--- scripts/install_julia.sh
+++ scripts/install_julia.sh
@@ -1,33 +1,50 @@
 #!/bin/bash
 set -e
 
-# Latest Julia version (this needs to be manually updated, periodically)
-JULIA_LATEST=1.6-latest
+## build ARGs
+NCPUS=${NCPUS:--1}
 
 JULIA_VERSION=${1:-${JULIA_VERSION:-latest}}
 
-if [ "$JULIA_VERSION" = "latest" ]; then
-    JULIA_VERSION=$JULIA_LATEST
+ARCH_LONG=$(uname -p)
+ARCH_SHORT=$ARCH_LONG
+
+if [ "$ARCH_LONG" = "x86_64" ]; then
+  ARCH_SHORT="x64"
 fi
-JULIA_MAJOR_VERSION=${JULIA_VERSION:0:3}
 
-# Download and extract Julia. We'll use a common /opt/julia/ location regardless of version 
-wget https://julialang-s3.julialang.org/bin/linux/x64/$JULIA_MAJOR_VERSION/julia-$JULIA_VERSION-linux-x86_64.tar.gz -O /opt/julia.tar.gz
+if [ ! -x "$(command -v wget)" ]; then
+  apt-get update
+  apt-get -y install wget
+fi
+
+install2.r --error --skipinstalled -n "$NCPUS" \
+  yaml \
+  JuliaCall \
+  JuliaConnectoR
+
+# Get the latest Julia version by using R and the R yaml package.
+if [ "$JULIA_VERSION" = "latest" ]; then
+  JULIA_VERSION=$(Rscript -e '
+js <- yaml::read_yaml("https://julialang-s3.julialang.org/bin/versions.json")
+versions <- names(js)
+is_stable <- unlist(Map(function(x) x$stable, js))
+latest_version <- sort(versions[is_stable], decreasing = TRUE)[1]
+cat(latest_version)
+')
+fi
+
+JULIA_MINOR_VERSION=${JULIA_VERSION:0:3}
+
+# Download Julia and create a symbolic link.
+wget "https://julialang-s3.julialang.org/bin/linux/${ARCH_SHORT}/${JULIA_MINOR_VERSION}/julia-${JULIA_VERSION}-linux-${ARCH_LONG}.tar.gz"
 mkdir /opt/julia
-tar zxvf /opt/julia.tar.gz -C /opt/julia --strip-components 1
-rm -rf /opt/julia.tar.gz
+tar zxvf "julia-${JULIA_VERSION}-linux-${ARCH_LONG}.tar.gz" -C /opt/julia --strip-components 1
+rm -f "julia-${JULIA_VERSION}-linux-${ARCH_LONG}.tar.gz"
+ln -s /opt/julia/bin/julia /usr/local/bin/julia
 
-# Add to path
-echo 'export PATH="/opt/julia/bin:$PATH"' >> .bashrc
-source .bashrc
+julia --version
 
-# Add RCall package for interoperability from Julia
-julia -e 'using Pkg; Pkg.add("RCall")'
-
-# Add R packages for interoperability the other way around
-#Rscript -e 'install.packages(c("JuliaCall", "JuliaConnectoR")); JuliaCall::julia_setup()'
-install2.r --error --skipinstalled JuliaCall JuliaConnectoR
-Rscript -e 'JuliaCall::julia_setup()'
-
-# Make sure we start in bash
-CMD ["bash"]
+# Clean up
+rm -rf /var/lib/apt/lists/*
+rm -rf /tmp/downloaded_packages
