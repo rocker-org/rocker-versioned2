29f8c549e4a4dd49e3cad8d0a73f9cd5fb661203
eitsupi <ts1s1andn@gmail.com>
2021-10-28 19:53:43 +0900

    update via upstream https://github.com/eddelbuettel/littler/commit/c895acc6196ab1bb87ceb9546c70072eb14f5814

--- scripts/bin/install2.r
+++ scripts/bin/install2.r
@@ -80,7 +80,8 @@ install_packages2 <- function(pkgs, ..., error = FALSE, skipmissing = FALSE, ski
             catch <-
                 grepl("download of package .* failed", e$message) ||
                 (grepl("(dependenc|package).*(is|are) not available", e$message) && !skipmissing) ||
-                grepl("installation of package.*had non-zero exit status", e$message)
+                grepl("installation of package.*had non-zero exit status", e$message) ||
+                grepl("installation of one or more packages failed", e$message)
             if (catch) {
                 e <<- e
             }
