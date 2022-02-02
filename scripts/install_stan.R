964faf9a78175d6d30f4b37b0e93042e3a4a67ce
Rob Young <young@ecohealthalliance.org>
2022-02-02 17:43:47 -0500

    changed stan install script to bash

--- scripts/install_stan.R
+++
@@ -1,6 +0,0 @@
-# install stan
-# users should run this script to install stan their R session
-
-install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
-library(cmdstanr)
-install_cmdstan()
