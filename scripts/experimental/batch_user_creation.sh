d16789b55fb8ff449a052227e14864d4975b0dbc
Rodrigo Laiola Guimarães <rlaiola@gmail.com>
2022-03-11 23:01:06 -0300

    Security fix, code improvements and tests

--- scripts/experimental/batch_user_creation.sh
+++ scripts/experimental/batch_user_creation.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 set -e
 
+# Remove spaces
+remove_spaces() {
+    local var="$*"
+    # Remove all spaces
+    var=${var//$' '/''}
+    echo -e "$var"
+    return 0
+}
+
 function create_user() {
   local username=$1
   local password=$2
+
+  echo "Processing user '${username}'."
+
   if id -u "$username" >/dev/null 2>&1; then
-    echo '$username user already exists'
+    echo "'${username}' user already exists. Nothing else to do."
   else
     useradd -s /bin/bash -m $username
+    # invalid user name
+    if [ "$?" == 3 ]; then
+      echo "Failed to create user '${username}'."
+      return
+    fi
+
+    if [ -z "$password" ]; then
+      echo "Password not provided. Setting it equals to username."
+      password=${username}
+    fi
     echo "${username}:${password}" | chpasswd
+    
     addgroup ${username} staff
 
     mkdir -p /home/${username}/.rstudio/monitored/user-settings
@@ -26,17 +49,24 @@ function create_user() {
   if [ -x "$(command -v shiny-server)" ]; then
     adduser ${username} shiny
   fi
+
+  echo "Done with user '${username}'."
 }
 
 if [ -n "$BATCH_USER_CREATION" ]; then
   echo "Requested creation of multiple user accounts in batch mode."
 
+  BATCH_USER_CREATION=`remove_spaces "$BATCH_USER_CREATION"`
+
   for user in $(echo $BATCH_USER_CREATION | tr ';' ' '); do
     IFS=: read username password <<< $(echo $user)
-    create_user $username $password
+
+    if [ -z "$username" ]; then
+      echo "Failed to create user: username undefined"
+      continue;
+    else
+      create_user $username $password || true
+    fi
   done
   echo "Finished creation of multiple user accounts in batch mode."
-
-  # For security reasons...
-  unset BATCH_USER_CREATION
 fi
