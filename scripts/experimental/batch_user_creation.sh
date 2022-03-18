f272d8ba4a71d2cf1f57f16b51e609df53533824
Rodrigo Laiola Guimarães <rlaiola@gmail.com>
2022-03-18 16:05:57 -0300

    Documentation on how to set up multiple users

--- scripts/experimental/batch_user_creation.sh
+++ scripts/experimental/batch_user_creation.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+# Batch creation of user accounts in RStudio server. The script reads a list
+# of username and password pairs from the `BATCH_USER_CREATION` enviroment 
+# variable and uses this information to update a group of existing users when
+# the container starts. Each pair is of the format: username:password and is
+# separated from the next by a semicolon, with no intervening whitespace.
+# Usernames may only be up to 32 characters long (required by `useradd`) and
+# by default the supplied passwords must be in clear-text (later encrypted by
+# `chpasswd). If an username already exists, the script will deny that 
+# particular account creation request; if not, the user account will be
+# created, the login shell set to Bash and the user's home directory created,
+# if it does not exist. By default, a group will be created for each new user
+# with the same name as her username. If the groupname already exists, the
+# script will deny the group creation request. If the password is not
+# specified, it will be assumed that it is equals to the username. All users
+# will also be added to the `staff` group (same as rstudio user). A directory
+# called .rstudio/monitored/user-settings/user-settings is created in that
+# users home directory to store RStudio initial preferences. Users are not
+# allowed to read other users' home directory.
+
 set -e
 
 # Remove spaces
