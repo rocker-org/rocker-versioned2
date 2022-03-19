2a97c0f2202e6a745b6acc684896934050625a90
eitsupi <ts1s1andn@gmail.com>
2022-03-19 19:44:32 +0900

    minor refactoring user agent header for RSPM

--- scripts/setup_R.sh
+++ scripts/setup_R.sh
@@ -16,9 +16,10 @@ fi
 echo "options(repos = c(CRAN = '${CRAN}'), download.file.method = 'libcurl')" >> ${R_HOME}/etc/Rprofile.site
 
 ## Set HTTPUserAgent for RSPM (https://github.com/rocker-org/rocker/issues/400)
-echo  'options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(),
-                 paste(getRversion(), R.version$platform,
-                       R.version$arch, R.version$os)))' >> ${R_HOME}/etc/Rprofile.site
+cat <<EOF >>"${R_HOME}/etc/Rprofile.site"
+# https://docs.rstudio.com/rspm/admin/serving-binaries/#binaries-r-configuration-linux
+options(HTTPUserAgent = sprintf("R/%s R (%s)", getRversion(), paste(getRversion(), R.version["platform"], R.version["arch"], R.version["os"])))
+EOF
 
 ## Use littler installation scripts
 Rscript -e "install.packages(c('littler', 'docopt'), repos='${CRAN_SOURCE}')"
