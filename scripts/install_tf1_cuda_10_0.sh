d968d4f5fead3e42cb9dda6fc808f7657098efad
eitsupi <ts1s1andn@gmail.com>
2022-03-29 20:17:45 +0900

    refactoring
    - fixed by shellcheck and shfmt
    - remove unused vars
    - use here document
    - remove unnecessary `&&`s

--- scripts/install_tf1_cuda_10_0.sh
+++ scripts/install_tf1_cuda_10_0.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -e
 
-# Tensorflow 1.x is required for numerous projects.  
-# Even the most recent of the 1.x, 1.15.5 is compatible only 
+# Tensorflow 1.x is required for numerous projects.
+# Even the most recent of the 1.x, 1.15.5 is compatible only
 # with CUDA 10.0 versions of the following libraries.
 
 # Fortunately, these are available from the NVIDIA Ubuntu debian PPA repos added in 10.1 images
 
 apt-get update && apt-get install -y \
-  cuda-cudart-10-0 \
-  cuda-cufft-10-0 \
-  cuda-cusolver-10-0 \
-  cuda-curand-10-0 \
-  cuda-cusparse-10-0 \
-  cuda-cublas-10-0
-
+    cuda-cudart-10-0 \
+    cuda-cufft-10-0 \
+    cuda-cusolver-10-0 \
+    cuda-curand-10-0 \
+    cuda-cusparse-10-0 \
+    cuda-cublas-10-0
