3ab1d1eb047354d889d4c012c969aea78bb47349
eitsupi <ts1s1andn@gmail.com>
2022-04-18 14:53:31 +0000

    export inline cache to all images

--- build/make-bakejson.R
+++ build/make-bakejson.R
@@ -47,7 +47,7 @@ library(stringr)
       ),
       platforms = purrr::map(value, "platforms", .default = list("linux/amd64")),
       `cache-from` = purrr::map(value, "cache-from", .default = list("")),
-      `cache-to` = purrr::map(value, "cache-to", .default = list("")),
+      `cache-to` = purrr::map(value, "cache-to", .default = list("type=inline")),
       base_image = map_chr(value, "FROM")
     ) |>
     dplyr::rowwise() |>
