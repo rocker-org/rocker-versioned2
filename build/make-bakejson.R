a8b9724a93518659c4a98e3516f7710f7d2418e8
eitsupi <ts1s1andn@gmail.com>
2022-04-18 14:51:13 +0000

    refactoring

--- build/make-bakejson.R
+++ build/make-bakejson.R
@@ -9,7 +9,7 @@ library(stringr)
 
 .image_full_name <- function(image_name) {
   full_name <- dplyr::case_when(
-    stringr::str_detect(image_name, "^.+\\..+/.+/.+") ~ image_name,
+    stringr::str_detect(image_name, r"(^.+\..+/.+/.+)") ~ image_name,
     stringr::str_count(image_name, "/") == 0 ~ stringr::str_c("docker.io/library/", image_name),
     stringr::str_count(image_name, "/") == 1 ~ stringr::str_c("docker.io/", image_name)
   )
@@ -21,7 +21,7 @@ library(stringr)
 
 
 .version_or_null <- function(tag) {
-  if (stringr::str_detect(tag, "^\\d+\\.\\d+\\.\\d+$")) {
+  if (stringr::str_detect(tag, r"(^\d+\.\d+\.\d+$)")) {
     return(stringr::str_c("R-", tag))
   } else {
     return(NULL)
@@ -29,36 +29,36 @@ library(stringr)
 }
 
 
-.bake_list <- function(stack_file) {
-  stack_content <- jsonlite::read_json(stack_file)
+.bake_list <- function(stack_file_path) {
+  stack_content <- jsonlite::read_json(stack_file_path)
 
   stack_tag <- stack_content$TAG
 
-  content_list <- stack_content$stack %>%
-    tibble::enframe() %>%
+  df_content <- stack_content$stack |>
+    tibble::enframe() |>
     dplyr::transmute(
       name = purrr::map_chr(value, "IMAGE", .default = NA),
       context = "./",
-      dockerfile = paste0("dockerfiles/", name, "_", stack_tag, ".Dockerfile"),
+      dockerfile = stringr::str_c("dockerfiles/", name, "_", stack_tag, ".Dockerfile"),
       labels = purrr::map(value, "labels"),
       tags = purrr::map(
         value,
-        ~ if (!is.null(.x$tags)) .x$tags else list(paste0("docker.io/rocker/", .x$IMAGE, ":", stack_tag))
+        ~ if (!is.null(.x$tags)) .x$tags else list(stringr::str_c("docker.io/rocker/", .x$IMAGE, ":", stack_tag))
       ),
       platforms = purrr::map(value, "platforms", .default = list("linux/amd64")),
       `cache-from` = purrr::map(value, "cache-from", .default = list("")),
       `cache-to` = purrr::map(value, "cache-to", .default = list("")),
       base_image = map_chr(value, "FROM")
-    ) %>%
-    dplyr::rowwise() %>%
+    ) |>
+    dplyr::rowwise() |>
     dplyr::mutate(
       labels = list(purrr::list_modify(
         labels,
         org.opencontainers.image.base.name = .image_full_name(base_image),
         org.opencontainers.image.version = .version_or_null(stack_tag)
       ))
-    ) %>%
-    dplyr::ungroup() %>%
+    ) |>
+    dplyr::ungroup() |>
     dplyr::select(
       name,
       context,
@@ -68,28 +68,31 @@ library(stringr)
       platforms,
       `cache-from`,
       `cache-to`
-    ) %>%
-    {
-      list(
-        group = if (!is.null(stack_content$group)) {
-          stack_content$group
-        } else {
-          list(c(list(default = list(c(list(targets = if (length(.$name) == 1) list(.$name) else c(.$name)))))))
-        },
-        target = purrr::transpose(dplyr::select(., !name), .names = .$name)
-      )
-    }
+    )
 
-  return(content_list)
+  list_content <- list(
+    group = if (!is.null(stack_content$group)) {
+      stack_content$group
+    } else {
+      list(c(list(
+        default = list(c(list(
+          targets = if (length(df_content$name) == 1) list(df_content$name) else c(df_content$name)
+        )))
+      )))
+    },
+    target = purrr::transpose(dplyr::select(df_content, !name), .names = df_content$name)
+  )
+
+  return(list_content)
 }
 
 
-write_bakejson <- function(stack_file) {
-  output_path <- sub("^stacks/", "bakefiles/", stack_file) %>% {
-    sub(".json$", ".docker-bake.json", .)
-  }
+write_bakejson <- function(stack_file_path) {
+  output_path <- stack_file_path |>
+    stringr::str_replace("^stacks/", "bakefiles/") |>
+    stringr::str_replace(".json$", ".docker-bake.json")
 
-  .bake_list(stack_file) %>%
+  .bake_list(stack_file_path) |>
     jsonlite::write_json(output_path, pretty = TRUE, auto_unbox = TRUE)
 
   message(output_path)
@@ -98,7 +101,7 @@ write_bakejson <- function(stack_file) {
 
 message("\nstart writing docker-bake.json files.")
 
-list.files(path = "stacks", pattern = "\\.json$", full.names = TRUE) %>%
+list.files(path = "stacks", pattern = "\\.json$", full.names = TRUE) |>
   purrr::walk(write_bakejson)
 
 message("make-bakejson.R done!\n")
