acda48e50987721da7a9820927d76998460cd3d4
eitsupi <ts1s1andn@gmail.com>
2022-05-01 09:00:30 +0000

    add installation of necessary packages

--- scripts/install_binder.sh
+++ scripts/install_binder.sh
@@ -3,6 +3,18 @@ set -e
 
 NB_USER=${NB_USER:-${DEFAULT_USER:-"rstudio"}}
 
+# a function to install apt packages only if they are not installed
+function apt_install() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt-get update
+        fi
+        apt-get install -y --no-install-recommends "$@"
+    fi
+}
+
+apt_install git sudo
+
 # set up the default user if it does not exist
 if ! id -u "${NB_USER}" >/dev/null 2>&1; then
     /rocker_scripts/default_user.sh "${NB_USER}"
@@ -28,3 +40,6 @@ pip3 install --no-cache-dir jupyter-rsession-proxy notebook jupyterlab >=2.0
 
 R --quiet -e "devtools::install_github('IRkernel/IRkernel')"
 R --quiet -e "IRkernel::installspec(prefix='${PYTHON_VENV_PATH}')"
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
