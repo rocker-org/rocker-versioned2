870e541ad5672245b5b8bf727ce73ebd80ab780f
Carl <cboettig@gmail.com>
2022-05-15 17:30:58 +0000

    shfmt format

--- scripts/dev_osgeo.sh
+++ scripts/dev_osgeo.sh
@@ -2,65 +2,67 @@
 set -e
 
 CRAN=${CRAN_SOURCE:-https://cran.r-project.org}
-echo "options(repos = c(CRAN = '${CRAN}'), download.file.method = 'libcurl')" >> "${R_HOME}/etc/Rprofile.site"
+echo "options(repos = c(CRAN = '${CRAN}'), download.file.method = 'libcurl')" >>"${R_HOME}/etc/Rprofile.site"
 
-export DEBIAN_FRONTEND=noninteractive; apt-get -y update \
-  && apt-get install -y \
-	gdb \
-	git \
-	libcairo2-dev \
-	libcurl4-openssl-dev \
-	libexpat1-dev \
-	libpq-dev \
-	libsqlite3-dev \
-	libudunits2-dev \
-	make \
-	pandoc \
-	qpdf \
-  	sqlite3 \
-	subversion \
-	valgrind \
-	vim \
-	tk-dev \
-	wget
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y update &&
+    apt-get install -y \
+        gdb \
+        git \
+        libcairo2-dev \
+        libcurl4-openssl-dev \
+        libexpat1-dev \
+        libpq-dev \
+        libsqlite3-dev \
+        libudunits2-dev \
+        make \
+        pandoc \
+        qpdf \
+        sqlite3 \
+        subversion \
+        valgrind \
+        vim \
+        tk-dev \
+        wget
 
 apt-get install -y \
-	libv8-dev \
-	libjq-dev \
-	libprotobuf-dev \
-	libxml2-dev \
-	libprotobuf-dev \
-	protobuf-compiler \
-	unixodbc-dev \
-	libssh2-1-dev \
-	libgit2-dev \
-	libnetcdf-dev \
-	locales \
-	libssl-dev \
-	libtiff-dev
+    libv8-dev \
+    libjq-dev \
+    libprotobuf-dev \
+    libxml2-dev \
+    libprotobuf-dev \
+    protobuf-compiler \
+    unixodbc-dev \
+    libssh2-1-dev \
+    libgit2-dev \
+    libnetcdf-dev \
+    locales \
+    libssl-dev \
+    libtiff-dev
 
 locale-gen en_US.UTF-8
 
 PROJ_VERSION=${PROJ_VERSION:-9.0.0}
 LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-export DEBIAN_FRONTEND=noninteractive; apt-get -y update \
-  && apt-get install -y \
-	cmake \
-	libtiff5-dev
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y update &&
+    apt-get install -y \
+        cmake \
+        libtiff5-dev
 
 #git clone --depth 1 https://github.com/OSGeo/PROJ.git
-wget http://download.osgeo.org/proj/proj-$PROJ_VERSION.tar.gz
-tar zxvf proj-${PROJ_VERSION}.tar.gz
-cd proj-${PROJ_VERSION} \
-  && ls -l \
-  && mkdir build \
-  && cd build \
-  && cmake .. \
-  && make \
-  && make install \
-  && cd ../.. \
-  && ldconfig
+wget http://download.osgeo.org/proj/proj-"${PROJ_VERSION}".tar.gz
+tar zxvf proj-"${PROJ_VERSION}".tar.gz
+cd proj-"${PROJ_VERSION}" &&
+    ls -l &&
+    mkdir build &&
+    cd build &&
+    cmake .. &&
+    make &&
+    make install &&
+    cd ../.. &&
+    ldconfig
 
 # install proj-data:
 #cd /usr/local/share/proj \
@@ -75,15 +77,15 @@ cd proj-${PROJ_VERSION} \
 GDAL_VERSION=${GDAL_VERSION:-3.5.0}
 GDAL_VERSION_NAME=${GDAL_VERSION}
 
-wget http://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION_NAME}.tar.gz \
-  && tar -xf gdal-${GDAL_VERSION_NAME}.tar.gz \
-  && rm *.tar.gz \
-  && cd gdal* \
-  && ./configure \
-  && make \
-  && make install \
-  && cd .. \
-  && ldconfig
+wget http://download.osgeo.org/gdal/"${GDAL_VERSION}"/gdal-"${GDAL_VERSION_NAME}".tar.gz &&
+    tar -xf gdal-"${GDAL_VERSION_NAME}".tar.gz &&
+    rm *.tar.gz &&
+    cd gdal* &&
+    ./configure &&
+    make &&
+    make install &&
+    cd .. &&
+    ldconfig
 
 #git clone --depth 1 https://github.com/OSGeo/gdal.git
 #cd gdal/gdal \
@@ -97,16 +99,16 @@ wget http://download.osgeo.org/gdal/${GDAL_VERSION}/gdal-${GDAL_VERSION_NAME}.ta
 # GEOS:
 GEOS_VERSION=${GEOS_VERSION:-3.10.2}
 
-wget http://download.osgeo.org/geos/geos-${GEOS_VERSION}.tar.bz2 \
-  && bzip2 -d geos-*bz2 \
-  && tar xf geos*tar \
-  && rm *.tar \
-  && cd geos* \
-  && ./configure \
-  && make \
-  && make install \
-  && cd .. \
-  && ldconfig
+wget http://download.osgeo.org/geos/geos-"${GEOS_VERSION}".tar.bz2 &&
+    bzip2 -d geos-*bz2 &&
+    tar xf geos*tar &&
+    rm ./*.tar &&
+    cd geos* &&
+    ./configure &&
+    make &&
+    make install &&
+    cd .. &&
+    ldconfig
 
 # svn  checkout svn://scm.r-forge.r-project.org/svnroot/rgdal/
 # R CMD build rgdal/pkg --no-build-vignettes
@@ -120,7 +122,10 @@ git clone --depth 10 https://github.com/r-spatial/stars.git
 #git config --global user.email "edzer.pebesma@uni-muenster.de"
 
 R CMD build --no-build-vignettes --no-manual lwgeom
-(cd sf; git pull)
+(
+    cd sf
+    git pull
+)
 R CMD build --no-build-vignettes --no-manual sf
 # pkg-config proj --modversion
 R CMD INSTALL sf
