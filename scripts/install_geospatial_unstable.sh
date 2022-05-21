6a08c97ee80a8d0db1954e0b87831ef8bf3050f6
eitsupi <ts1s1andn@gmail.com>
2022-05-21 05:42:41 +0000

    use the add-apt-repository command to add ppa

--- scripts/install_geospatial_unstable.sh
+++ scripts/install_geospatial_unstable.sh
@@ -16,13 +16,13 @@ function apt_install() {
 # always set this for scripts but don't declare as ENV..
 export DEBIAN_FRONTEND=noninteractive
 apt_install \
+  software-properties-common \
   gnupg2 \
   curl \
   ca-certificates
 
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B827C12C2D425E227EDCA75089EBE08314DF160
-echo "deb http://ppa.launchpad.net/ubuntugis/ubuntugis-unstable/ubuntu focal main" >> /etc/apt/sources.list.d/ubuntugis.list
-echo "deb-src http://ppa.launchpad.net/ubuntugis/ubuntugis-unstable/ubuntu focal main" >> /etc/apt/sources.list.d/ubuntugis.list
+# Adding the ubuntugis-unstable PPA
+add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 
 ## in UNSTABLE, we will install everything from source by default:
 echo "options(repos = c(CRAN = '${CRAN}'))" >>"${R_HOME}/etc/Rprofile.site"
