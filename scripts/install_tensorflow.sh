d29104ce16b45eca5ab682371f248f4dc1283a7f
eitsupi <ts1s1andn@gmail.com>
2022-06-25 15:33:16 +0000

    remove unused ENVs

--- scripts/install_tensorflow.sh
+++ scripts/install_tensorflow.sh
@@ -4,9 +4,6 @@ set -e
 ## build ARGs
 NCPUS=${NCPUS:--1}
 
-TENSORFLOW_VERSION=${1:-${TENSORFLOW_VERSION:-default}}
-KERAS_VERSION=${2:-${KERAS_VERSION:-default}}
-
 ## Install python dependency
 /rocker_scripts/install_python.sh
 
