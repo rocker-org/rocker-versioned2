5de1f7dcadbeda717a6b79cc047237d38963aa1a
Salim B <git@salim.space>
2022-11-03 01:52:40 +0000

    Improve script description

--- scripts/experimental/batch_user_creation.sh
+++ scripts/experimental/batch_user_creation.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Batch creation of user accounts in RStudio server. The script reads a list
-# of username and password pairs from the `BATCH_USER_CREATION` enviroment
+# Batch creation of user accounts in RStudio Server. The script reads a list
+# of username and password pairs from the `BATCH_USER_CREATION` environment
 # variable and uses this information to update a group of existing users when
-# the container starts. Each pair is of the format: username:password and is
+# the container starts. Each pair is of the format `username:password` and is
 # separated from the next by a semicolon, with no intervening whitespace.
 # Usernames may only be up to 32 characters long (required by `useradd`) and
 # by default the supplied passwords must be in clear-text (later encrypted by
-# `chpasswd`). If an username already exists, the script will deny that
-# particular account creation request; if not, the user account will be
-# created, the login shell set to Bash and the user's home directory created,
-# if it does not exist. By default, a group will be created for each new user
-# with the same name as her username. If the groupname already exists, the
-# script will deny the group creation request. If the password is not
-# specified, it will be assumed that it is equals to the username. All users
-# will also be added to the `staff` group (same as rstudio user). A directory
-# called .rstudio/monitored/user-settings/user-settings is created in that
-# users home directory to store RStudio initial preferences. Users are not
+# `chpasswd`). If a username already exists, the script will deny that
+# particular account creation request; otherwise, the user account will be
+# created, the login shell set to Bash and the user's home directory created
+# unless it already exists. By default, a group will be created for each new
+# user with the same name as her username. If the groupname already exists,
+# the script will deny the group creation request. If the password is not
+# specified, it is assumed to be equal to the username. All users will also be
+# added to the `staff` group (same as the default `rstudio` user). A directory
+# called `.rstudio/monitored/user-settings/user-settings` is created in that
+# user's home directory to store RStudio initial preferences. Users are not
 # allowed to read other users' home directory.
 
 set -e
