d78c3150067acda577cef1e7dadcec8be70ce9f0
Robert Denham <rjadenham@gmail.com>
2022-12-12 11:33:39 +1000

    Continue installing wgrib2 but on focal images only

--- scripts/install_geospatial.sh
+++ scripts/install_geospatial.sh
@@ -77,6 +77,14 @@ install2.r --error --skipmissing --skipinstalled -n "$NCPUS" \
 
 R -e "BiocManager::install('rhdf5')"
 
+## install wgrib2 for NOAA's NOMADS / rNOMADS forecast files
+## This is no longer needed, but we include it for reproducibility
+## reasons on earlier base images.
+source /etc/os-release
+if [ "${UBUNTU_CODENAME}" == "focal" ]; then
+    /rocker_scripts/install_wgrib2.sh
+fi
+
 # Clean up
 rm -rf /var/lib/apt/lists/*
 rm -r /tmp/downloaded_packages
