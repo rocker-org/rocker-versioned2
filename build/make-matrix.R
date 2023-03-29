1718628674bec47c19776089ebbbb9970243ce7c
eitsupi <ts1s1andn@gmail.com>
2023-03-29 11:26:48 +0000

    only build last 2 minor or last patch of each minor

--- build/make-matrix.R
+++ build/make-matrix.R
@@ -34,10 +34,30 @@ df_args <- fs::dir_ls(path = "bakefiles", regexp = "/\\d+\\.\\d+\\.\\d+\\.docker
   ) |>
   dplyr::arrange(R_system_version(r_version))
 
+supported_versions <- df_args |>
+  dplyr::select(r_version) |>
+  dplyr::distinct() |>
+  dplyr::mutate(
+    sys_ver = R_system_version(r_version),
+    major = sys_ver$major,
+    minor = sys_ver$minor,
+    patch = sys_ver$patch
+  ) |>
+  dplyr::mutate(
+    supported = (patch == dplyr::last(patch)),
+    .by = c(major, minor)
+  ) |>
+  dplyr::mutate(
+    major_minor = numeric_version(paste0(major, ".", minor)),
+    supported = supported | (major_minor >= nth(unique(major_minor), -2))
+  ) |>
+  dplyr::filter(supported) |>
+  dplyr::pull(r_version)
 
 message("\nstart writing matrix files.")
 
 df_args |>
+  dplyr::filter(r_version %in% supported_versions) |>
   .write_matrix("build/matrix/all.json")
 
 df_args |>
