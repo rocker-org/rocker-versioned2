3345f6ef3ad6fc01f8fe59d5c70777219fb7bf88
Carl Boettiger <cboettig@gmail.com>
2023-08-22 20:42:41 -0700

    Update install_dev_osgeo.sh
    Support for zarr format needs blosc compression.
    
    osgeo-dev should also be sure other common compression libs are included.  
    
    /ht @mdsumner, thanks!

--- scripts/experimental/install_dev_osgeo.sh
+++ scripts/experimental/install_dev_osgeo.sh
@@ -71,8 +71,12 @@ apt_install \
     cmake \
     libtiff5-dev \
     libhdf4-alt-dev \
-    libhdf5-dev
-
+    libhdf5-dev \
+    liblzma-dev \
+    libblosc-dev \
+    libzstd-dev \
+    libz4-dev
+    
 ## geoparquet support
 wget https://apache.jfrog.io/artifactory/arrow/"$(lsb_release --id --short | tr '[:upper:]' '[:lower:]')"/apache-arrow-apt-source-latest-"$(lsb_release --codename --short)".deb
 apt_install -y -V ./apache-arrow-apt-source-latest-"$(lsb_release --codename --short)".deb
