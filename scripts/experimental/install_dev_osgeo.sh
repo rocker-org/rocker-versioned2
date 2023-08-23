17924556498c2b642d279d2934621a6541178f66
Carl Boettiger <cboettig@gmail.com>
2023-08-22 20:55:00 -0700

    Update install_dev_osgeo.sh

--- scripts/experimental/install_dev_osgeo.sh
+++ scripts/experimental/install_dev_osgeo.sh
@@ -75,7 +75,7 @@ apt_install \
     liblzma-dev \
     libblosc-dev \
     libzstd-dev \
-    libz4-dev
+    liblz4-dev
     
 ## geoparquet support
 wget https://apache.jfrog.io/artifactory/arrow/"$(lsb_release --id --short | tr '[:upper:]' '[:lower:]')"/apache-arrow-apt-source-latest-"$(lsb_release --codename --short)".deb
