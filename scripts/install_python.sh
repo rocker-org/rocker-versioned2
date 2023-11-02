c1a472465532c8349cf0ad4f3f95c86e85787dc3
YuviPanda <yuvipanda@gmail.com>
2023-11-02 23:40:42 +0530

    Make sure RStudio sees new env vars we set

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -28,6 +28,10 @@ apt_install \
 export VIRTUAL_ENV="${VIRTUAL_ENV:=/opt/venv}"
 export PATH="${VIRTUAL_ENV}/bin:${PATH}"
 
+# Make sure that Rstudio sees these env vars too
+echo "PATH=${PATH}" >>"${R_HOME}/etc/Renviron.site"
+echo "VIRTUAL_ENV=${VIRTUAL_ENV}" >>"${R_HOME}/etc/Renviron.site"
+
 python3 -m venv "${VIRTUAL_ENV}"
 
 # Upgrade version of pip inside the virtualenv
