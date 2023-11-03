c16cf1ba5f6796dc7fe5afd36d3e7e9b0d2b8d30
Carl Boettiger <cboettig@gmail.com>
2023-11-02 19:30:03 -0700

    Updated OSGeo recipes (#708)
    This provides a more complete source-build of OSGeo with wider support
    for optional drivers. Notably, this addresses functionality that was
    already present in the Ubuntu LTS repo versions but missing from the
    current osgeo source-build recipe, such as virtual filesystem access for
    NetCDF.
    
    A few other changes are made here as well:
    
    - this now installs only the packages that actually bind OSGeo libraries
    from source, rather than installing all the packages listed in
    `install_geospatial.sh`. This also does not disable binary installs by
    default, meaning that users can benefit from quick binary installs for
    most other (spatial or non-spatial) R packages. The downside risk is
    that installing/upgrading a binary version of `sf`, `terra`,
    `gdalcubes`, or `stars` will create a broken installation.
    
    Still some open points for discussion:
    
    
    - [ ] some users suggested that having the recipe in
    `scripts/experimental` rather than `scripts` was discouraging for
    regular use. We have never outlined a policy for promoting a script from
    'experimental', and changing those paths could potentially break
    downstream functionality.
    
    - [ ] We might want to tweak the recipe of the corresponding
    `rocker/geospatial:dev-osgeo` Dockerfile to include `install_python.sh`
    step first? (most of those python libraries are in fact included here).
    
    - [ ] `sf` & `terra` do not install when PROJ is available both from a
    source build (i.e. `/usr/local/lib`) and from the deb repos (i.e.
    `/usr/lib`). Having different versions of a library like this is not a
    problem for the OSGeo libraries or for most software afaik
    (@eddelbuettel am I right this isn't usually an issue since config
    should link to only one version?), but apparently a well-documented
    problem for `sf` and `terra`. This can create some unexpected problems
    for users -- if they install any deb that happens to depend on
    `libproj-dev`, suddenly these R packages won't build from source (they
    will simply through a segfault on the configure step, leaving users no
    idea for the reason). Maybe we just live with this, but I thought it was
    worth flagging. (This script will first try and remove those libs to
    avoid conflicts).
    
    - [ ] Perhaps this entire script should be done as a 'staged build',
    where the resulting binaries of both the osgeo libs, cli tools, and
    binary versions of the R packages are copied over, but all the dev libs
    needed to compile are jettisoned (as the official osgeo/gdal Dockerfile
    does). That would create a much smaller Docker image, but adds
    complexity (i.e. users couldn't upgrade `sf`/`terra` etc even with a
    source-install, they'd have to run this whole script again first!), and
    complexity in our build setup.
    
    - [ ] Users can run this script to add latest osgeo builds to any image
    in the stack (i.e. run
    `/rocker_scripts/experimental/install_dev-osgeo.sh` on any
    rocker-versioned image). This should install the requested version,
    defaulting to the latest release. Perhaps this script should be called
    `install_osgeo-latest.sh` instead?
    
    - [ ] Wondering if it would make more sense to create a renv "profile"
    and install these R packages binding OSGeo libraries under that profile?
    This *might* make it possible to have binary and non-binary builds on
    the same image (could even be a way to have latest OSGeo versions
    available on the standard `rocker/geospatial` image). But I think that
    approach needs some more testing first.
    
    cc @mdsumner, @yuvipanda, @Robinlovelace, others -- thoughts?
    
    ---------
    
    Co-authored-by: eitsupi <ts1s1andn@gmail.com>

--- scripts/experimental/install_dev_osgeo.sh
+++ scripts/experimental/install_dev_osgeo.sh
@@ -12,7 +12,7 @@ PROJ_VERSION=${PROJ_VERSION:-"latest"}
 GDAL_VERSION=${GDAL_VERSION:-"latest"}
 GEOS_VERSION=${GEOS_VERSION:-"latest"}
 
-CRAN=${CRAN_SOURCE:-"https://cloud.r-project.org"}
+CRAN_SOURCE=${CRAN_SOURCE:-"https://cloud.r-project.org"}
 echo "options(repos = c(CRAN = '${CRAN}'))" >>"${R_HOME}/etc/Rprofile.site"
 
 # cmake does not understand "-1" as "all cpus"
@@ -31,56 +31,116 @@ function apt_install() {
     fi
 }
 
+# a function to remove apt packages only if they are installed
+function apt_remove() {
+    if dpkg -s "$@" >/dev/null 2>&1; then
+        apt-get remove -y "$@"
+    fi
+}
+
 function url_latest_gh_released_asset() {
     wget -qO- "https://api.github.com/repos/$1/releases/latest" | grep -oP "(?<=\"browser_download_url\":\s\")https.*\.tar.gz(?=\")" | head -n 1
 }
 
 export DEBIAN_FRONTEND=noninteractive
-apt_install \
-    gdb \
-    git \
-    lsb-release \
-    libcairo2-dev \
-    libcurl4-openssl-dev \
-    libexpat1-dev \
-    libpq-dev \
-    libsqlite3-dev \
-    libudunits2-dev \
-    make \
-    pandoc \
-    qpdf \
-    sqlite3 \
-    subversion \
-    valgrind \
-    vim \
-    tk-dev \
-    wget \
-    libv8-dev \
-    libjq-dev \
-    libprotobuf-dev \
-    libxml2-dev \
-    libprotobuf-dev \
-    protobuf-compiler \
-    unixodbc-dev \
-    libssh2-1-dev \
-    libgit2-dev \
-    libnetcdf-dev \
-    locales \
-    libssl-dev \
-    libtiff-dev \
-    cmake \
-    libtiff5-dev \
-    libhdf4-alt-dev \
-    libhdf5-dev \
-    liblzma-dev \
-    libblosc-dev \
-    libzstd-dev \
-    liblz4-dev
 
+apt_remove gdal-bin libgdal-dev libgeos-dev libproj-dev &&
+    apt-get autoremove -y
+
+## Derived from osgeo/gdal
+apt-get update
+apt-get install -y --fix-missing --no-install-recommends \
+    ant \
+    autoconf \
+    automake \
+    bash-completion \
+    build-essential \
+    ca-certificates \
+    cmake \
+    curl \
+    git \
+    libarchive-dev \
+    libarmadillo-dev \
+    libblosc-dev \
+    libboost-dev \
+    libbz2-dev \
+    libcairo2-dev \
+    libclc-15-dev \
+    libcfitsio-dev \
+    libcrypto++-dev \
+    libcurl4-openssl-dev \
+    libdeflate-dev \
+    libexpat-dev \
+    libfreexl-dev \
+    libfyba-dev \
+    libgif-dev \
+    libheif-dev \
+    libhdf4-alt-dev \
+    libhdf5-serial-dev \
+    libjpeg-dev \
+    libkml-dev \
+    liblcms2-2 \
+    liblerc-dev \
+    liblz4-dev \
+    liblzma-dev \
+    libmysqlclient-dev \
+    libnetcdf-dev \
+    libogdi-dev \
+    libopenexr-dev \
+    libopenjp2-7-dev \
+    libpcre3-dev \
+    libpng-dev \
+    libpq-dev \
+    libpoppler-dev \
+    libpoppler-private-dev \
+    libqhull-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libtiff5-dev \
+    libudunits2-dev \
+    libwebp-dev \
+    libxerces-c-dev \
+    libxml2-dev \
+    lsb-release \
+    make \
+    mdbtools-dev \
+    pkg-config \
+    python3-dev \
+    python3-numpy \
+    python3-setuptools \
+    sqlite3 \
+    swig \
+    unixodbc-dev \
+    wget \
+    zlib1g-dev
 ## geoparquet support
 wget https://apache.jfrog.io/artifactory/arrow/"$(lsb_release --id --short | tr '[:upper:]' '[:lower:]')"/apache-arrow-apt-source-latest-"$(lsb_release --codename --short)".deb
 apt_install -y -V ./apache-arrow-apt-source-latest-"$(lsb_release --codename --short)".deb
 apt-get update && apt-get install -y -V libarrow-dev libparquet-dev libarrow-dataset-dev
+
+rm -rf /build_local
+mkdir /build_local && cd /build_local
+
+## tiledb
+GCC_ARCH="$(uname -m)"
+export TILEDB_VERSION=2.16.3
+apt-get update -y &&
+    DEBIAN_FRONTEND=noninteractive apt-get install -y \
+        libspdlog-dev &&
+    mkdir tiledb &&
+    wget -q https://github.com/TileDB-Inc/TileDB/archive/${TILEDB_VERSION}.tar.gz -O - |
+    tar xz -C tiledb --strip-components=1 &&
+    cd tiledb &&
+    mkdir build_cmake &&
+    cd build_cmake &&
+    ../bootstrap --prefix=/usr --disable-werror &&
+    make "-j$(nproc)" &&
+    make install-tiledb DESTDIR="/build_thirdparty" &&
+    make install-tiledb &&
+    cd ../.. &&
+    rm -rf tiledb &&
+    for i in /build_thirdparty/usr/lib/"${GCC_ARCH}"-linux-gnu/*; do strip -s "$i" 2>/dev/null || /bin/true; done &&
+    for i in /build_thirdparty/usr/bin/*; do strip -s "$i" 2>/dev/null || /bin/true; done
 
 LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
@@ -103,6 +163,7 @@ cd build
 cmake ..
 cmake --build . --parallel "$CMAKE_CORES" --target install
 ldconfig
+cd /build_local
 
 # install proj
 # https://download.osgeo.org/proj/
@@ -120,8 +181,8 @@ mkdir build
 cd build
 cmake ..
 cmake --build . --parallel "$CMAKE_CORES" --target install
-cd ../..
 ldconfig
+cd /build_local
 
 # install gdal
 # https://download.osgeo.org/gdal/
@@ -141,37 +202,16 @@ cd ./build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --parallel "$CMAKE_CORES" --target install
 ldconfig
+cd /build_local
 
-install2.r --error --skipmissing --skipinstalled -n "$NCPUS" \
-    sp \
-    rgeos \
-    rgdal \
-    RPostgreSQL \
-    RSQLite \
-    testthat \
-    knitr \
-    tidyr \
-    geosphere \
-    maptools \
-    maps \
-    microbenchmark \
-    raster \
-    dplyr \
-    tibble \
-    units \
-    DBI \
-    covr \
-    protolite \
-    tmap \
-    mapview \
-    odbc \
-    pool \
-    rmarkdown \
-    RPostgres \
-    spatstat \
+apt-get update && apt-get -y install cargo
+
+install2.r --error --skipmissing -n "$NCPUS" -r "${CRAN_SOURCE}" \
     sf \
+    terra \
     lwgeom \
-    stars
+    stars \
+    gdalcubes
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
@@ -180,7 +220,5 @@ rm -rf /tmp/downloaded_packages
 # Check the geospatial packages
 
 echo -e "Check the stars package...\n"
-
 R -q -e "library(stars)"
-
 echo -e "\nInstall stars package, done!"
