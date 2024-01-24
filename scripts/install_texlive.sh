e09ba25f095b384dda42f00df84f5e76c6f841c3
Carl <cboettig@gmail.com>
2024-01-24 13:00:41 -0800

    set -e first

--- scripts/install_texlive.sh
+++ scripts/install_texlive.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+set -e
 
 if [[ -x "/usr/bin/latex" ]]; then
     echo "texlive already installed"
     exit 0
 fi
-
-set -e
 
 CTAN_REPO=${1:-${CTAN_REPO:-"https://mirror.ctan.org/systems/texlive/tlnet"}}
 
