07cb0ed53cf0a8e513914a96c5d7552caea5c597
Carl <cboettig@gmail.com>
2024-01-28 12:10:30 -0800

    lint

--- scripts/install_ml-cuda.sh
+++ scripts/install_ml-cuda.sh
@@ -3,7 +3,6 @@
 # always set this for scripts but don't declare as ENV..
 export DEBIAN_FRONTEND=noninteractive
 
-
 # a function to install apt packages only if they are not installed
 function apt_install() {
     if ! dpkg -s "$@" >/dev/null 2>&1; then
@@ -18,10 +17,8 @@ apt_install pciutils
 
 python3 -m pip install --no-cache-dir torch tensorflow[and-cuda] keras
 
-install2.r --error --skipmissing --skipinstalled torch tensorflow keras 
+install2.r --error --skipmissing --skipinstalled torch tensorflow keras
 
 # Clean up
 rm -rf /var/lib/apt/lists/*
 rm -r /tmp/downloaded_packages
-
-
