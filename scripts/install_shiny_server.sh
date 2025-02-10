00968c23a239af73b2ec90c8e230e86833c217eb
stibemueller <103206661+stibemueller@users.noreply.github.com>
2025-02-11 00:24:27 +0100

    fix path to download latest shiny-server
    Download url changed from download3.rstudio.org/ubuntu-18.04/... to download3.rstudio.org/ubuntu-20.04/...  (rocker-org/rocker-versioned2#904)

--- scripts/install_shiny_server.sh
+++ scripts/install_shiny_server.sh
@@ -33,10 +33,10 @@ apt_install \
 # Install Shiny server
 
 if [ "$SHINY_SERVER_VERSION" = "latest" ]; then
-    SHINY_SERVER_VERSION=$(wget -qO- https://download3.rstudio.org/ubuntu-18.04/x86_64/VERSION)
+    SHINY_SERVER_VERSION=$(wget -qO- https://download3.rstudio.org/ubuntu-20.04/x86_64/VERSION)
 fi
 
-wget --no-verbose "https://download3.rstudio.org/ubuntu-18.04/x86_64/shiny-server-${SHINY_SERVER_VERSION}-amd64.deb" -O ss-latest.deb
+wget --no-verbose "https://download3.rstudio.org/ubuntu-20.04/x86_64/shiny-server-${SHINY_SERVER_VERSION}-amd64.deb" -O ss-latest.deb
 gdebi -n ss-latest.deb
 rm ss-latest.deb
 
