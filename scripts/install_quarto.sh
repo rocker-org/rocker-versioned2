9b28e69461c6417ff1b71ce93c6a5f31b6ddd08c
Balazs Banfai <5557093+banfai@users.noreply.github.com>
2025-03-25 17:25:18 +0100

    fix(install_quarto.sh): release option works (#917)
    Fixes #916

--- scripts/install_quarto.sh
+++ scripts/install_quarto.sh
@@ -54,7 +54,7 @@ if [ "$QUARTO_VERSION" != "$INSTALLED_QUARTO_VERSION" ]; then
     if [ "$QUARTO_VERSION" = "$BUNDLED_QUARTO_VERSION" ] || [ "$QUARTO_VERSION" = "default" ]; then
         ln -fs "$BUNDLED_QUARTO" /usr/local/bin
     else
-        if [ "$QUARTO_VERSION" = "latest" ] || [ "$RSTUDIO_VERSION" = "release" ]; then
+        if [ "$QUARTO_VERSION" = "latest" ] || [ "$QUARTO_VERSION" = "release" ]; then
             QUARTO_DL_URL=$(wget -qO- https://quarto.org/docs/download/_download.json | grep -oP "(?<=\"download_url\":\s\")https.*${ARCH}\.deb")
         elif [ "$QUARTO_VERSION" = "prerelease" ]; then
             QUARTO_DL_URL=$(wget -qO- https://quarto.org/docs/download/_prerelease.json | grep -oP "(?<=\"download_url\":\s\")https.*${ARCH}\.deb")
